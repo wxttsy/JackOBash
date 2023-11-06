9f9d1189268e55d367aeb3472869b93185c37e31
Jemma Day <132528909+TimJamTam@users.noreply.github.com>
2023-11-06 14:40:23 +1100

    Ui Title Screen Added
    I added a Main menu scene with a image background, text title and 3 buttons
    - Start loads Game
    - Quit, Quits Game
    - Options doesnt do anything yet
    
    Ive also shuffled the build settings so Main menu loads first and debug actually loads as a build

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    //loads a manually inputed scene by name
+    public void LoadSceneByName()
+    {
+        SceneManager.LoadScene("DebugScene");
+    }
+    
+    //loads the next scene in build settings
+    //will be updated so we can randomly load scenes
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    //Quits game intended to use on a button
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
