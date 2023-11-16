ac842f5d54fce80493dfae742471e1e586b0ff0f
Jemma Day <132528909+TimJamTam@users.noreply.github.com>
2023-11-17 10:24:26 +1100

    Options/Settings now in Debug scene
    i got the Settings menu to work inside of debug.
    i found the problem came from having a script physically inside the canvas so when the canvas/panel is deactivating the script also is causing things to go haywire.

--- Assets/Scripts/Ui/OptionsMenu.cs
+++ Assets/Scripts/Ui/OptionsMenu.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject optionsMenu;
-    public GameObject UIGroup;
-   // public GameObject Menu2;
+    public GameObject Ui1;
+    public GameObject Ui2;
 
     // Start is called before the first frame update
     void Awake ()
     {
-        UIGroup.SetActive (true);
+     
         optionsMenu.SetActive (false);
     }
 
@@ -23,7 +24,8 @@ public class OptionsMenu : MonoBehaviour
 
     public void onClickOptions()
     {
-        UIGroup.SetActive(false);
+        Ui1.SetActive(false);
+        Ui2.SetActive(false);
         optionsMenu.SetActive(true);
         Time.timeScale = 0f;
         Debug.Log("onClickOptions CLicked");
@@ -32,7 +34,8 @@ public class OptionsMenu : MonoBehaviour
 
     public void BackButton()
     {
-        UIGroup.SetActive(true);
+        Ui1.SetActive(true);
+        Ui2.SetActive(true);
         optionsMenu.SetActive(false);
         Time.timeScale = 0f;
         Debug.Log("BACK CLICKED");
