1070de94a56e7fbc46e8ca24270fa24ae8f684ab
Sarah Watts <135098414+wxttsy@users.noreply.github.com>
2023-11-16 16:34:40 +1100

    Update
    Updated Comments and organized all scripts. Removed redundant and unused code/scripts and methods.
    Renamed scripts to match what they are doing since changes discussed with Tom, Scott and Jemma.
    
    Merged CandyManager, EffectsManager and RoomManager into one script now called GameManager.
    
    Work done with Tom-
    Parented enemies to their spawners so they are removed when a room is deleted.
    
    Health bar now increments during sugar rush and decrements otherwise by 1 second.
    
    Changed item prefabs to an array and when an enemy is killed and an item is meant to drop, it will choose a random item from that array of item prefabs.
    
    Basic Framework for PartyPopper ability added as a prefab.(Needs to be finished)
    
    Fixed destroying of parent object for partypopper - Sarah
    
    BUG: PumpkinCrawler is stretching weird when going over a table?
    Co-Authored-By: GaiVaige <127663263+GaiVaige@users.noreply.github.com>

---
+++ Assets/Scripts/ItemStuff/PartyPopper.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// Party popper quickly appears and disapears, the DestroyThis() function is for the animation event when it
+/// comes time to implement in the final release, and the timer will be deleted at the same time.
+/// 
+/// </summary>
+public class PartyPopper : MonoBehaviour
+{
+    BoxCollider _damageCollider;
+    public float timeToBeActive;
+    public float timeActive;
+
+    void Start()
+    {
+        transform.parent = FindObjectOfType<PlayerInput>().gameObject.transform;
+        _damageCollider = GetComponentInChildren<BoxCollider>();
+
+        timeActive = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeActive += Time.deltaTime;
+
+        if(timeActive> timeToBeActive)
+        {
+            DestroyThis();
+        }
+
+
+    }
+
+    public void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Ui/BarDisplayUI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BarDisplayUI : MonoBehaviour
+{
+    //*******************************************************************************************************************
+    //------------------------------------------Initialize Variables-----------------------------------------------------
+    //*******************************************************************************************************************
+    // References:
+    public TMP_Text healthText;
+    public Slider healthSlider;
+    public Slider sugarRushSlider;
+    private GameObject player;
+    Health playerHealthScript;
+
+    private float timePlayed = 0;
+
+    //*******************************************************************************************************************
+    //---------------------------------------------------Awake-----------------------------------------------------------
+    //*******************************************************************************************************************
+    private void Awake(){
+        timePlayed = 0;
+        player = GameObject.FindWithTag("Player");
+        playerHealthScript = player.GetComponent<Health>();
+        playerHealthScript.currentHealth = playerHealthScript.maxHealth;
+    }
+
+    //*******************************************************************************************************************
+    //---------------------------------------------------Update----------------------------------------------------------
+    //*******************************************************************************************************************
+    private void Update() {
+        timePlayed += 1 * Time.deltaTime;
+
+        // Do not decrement the player's health if it is going to be less than 0: Instead display 0.
+        if (playerHealthScript.currentHealth < 0){
+            playerHealthScript.currentHealth = 0;
+            healthText.text = "0";
+            healthSlider.value = (int)playerHealthScript.currentHealth;
+            return;
+        }
+        // Do not increment the player's health if it is going to be more than the max health value: Instead display max health value.
+        if (playerHealthScript.currentHealth > playerHealthScript.maxHealth) {
+            playerHealthScript.currentHealth = playerHealthScript.maxHealth;
+            healthText.text = "" + playerHealthScript.maxHealth;
+            healthSlider.value = (int)playerHealthScript.currentHealth;
+            UpdateSugarRushTimer();
+            return;
+        }
+        //Otherwise, increment appropriately.
+        UpdateSugarRushTimer();
+        healthText.text = "" + ((int)playerHealthScript.currentHealth+1) + "/" + playerHealthScript.maxHealth;
+        healthSlider.value = (int)playerHealthScript.currentHealth;
+    }
+    void UpdateSugarRushTimer()
+    {
+        Player playerScript = player.GetComponent<Player>();
+        if (playerScript.sugarRushIsActivated)
+        {
+            playerHealthScript.currentHealth += 1 * Time.deltaTime;
+        }
+        else
+        {
+            playerHealthScript.currentHealth -= 1 * Time.deltaTime;
+        }
+    }
+}
