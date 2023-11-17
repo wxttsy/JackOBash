01a3b9c72eeb6cff65cd632ffca6fbda17a5f472
Sarah Watts <114769566+Sylbie@users.noreply.github.com>
2023-11-17 14:36:48 +1100

    Outline Update/Known issue
    Pumpkin crawler model is still stretching when you get close, haven't looked into it properly yet but I am aware of the issue.

--- Assets/Scripts/EnemyStuff/PumpkinCrawler.cs
+++ Assets/Scripts/EnemyStuff/PumpkinCrawler.cs
@@ -27,7 +27,7 @@ public class PumpkinCrawler : MonoBehaviour
         }
         else if (Vector3.Distance(targetPlayer.position, transform.position) < navMeshAgent.stoppingDistance + 2)
         {
-            navMeshAgent.SetDestination(transform.position + Vector3.back);
+            navMeshAgent.SetDestination(targetPlayer.position);
         }
         // Update Rotation to face the direction immediately
         Quaternion newRotation = Quaternion.LookRotation(targetPlayer.position - transform.position);
