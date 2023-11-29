8a4d12e69be1fca84ee7d06c91256e73e2bfcf30
Sarah Watts <135098414+wxttsy@users.noreply.github.com>
2023-11-29 16:17:34 +1100

    Menu UI controller support
    In progress: Jemma will finish this tonight. Some buttons have not been updated with its on click events. Options and Death panels do not have a default selected button. this needs to be implemented.
    
    Co-Authored-By: Jemma Day <132528909+TimJamTam@users.noreply.github.com>

--- Assets/Scripts/Ui/UiManager.cs
+++ Assets/Scripts/Ui/UiManager.cs
@@ -1,13 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
-   
+    [SerializeField] EventSystem eventSystem;
+    [SerializeField] GameObject mainMenuFirst;
 
-    //loads a manually inputed scene by name
+
+    //DeathScript
+    public TMP_Text finalScore;
+    GameObject player;
+    Player playerScript;
+    public GameObject deathUI;
+    //DeathScript
+
+    //PauseMenu
+    public static bool GamePaused = false;
+    public static bool wasPaused = false;
+    public GameObject pauseMenuUI;
+    //PauseMenu
+
+    //OptionsMenu
+    public GameObject optionsMenu;
+    public GameObject Ui1;
+    //OptionsMenu
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+        if (player != null)
+            playerScript = player.GetComponent<Player>();
+
+        Resume();
+        if (optionsMenu != null)
+        {
+            optionsMenu.SetActive(false);
+        }
+    }
+    private void Start()
+    {
+        eventSystem.firstSelectedGameObject = mainMenuFirst;
+
+        if (deathUI != null)
+        {
+            deathUI.SetActive(false);
+        }
+
+
+    }
+
+    void Update()
+    {   
+        if (player != null)
+        {
+            finalScore.text = "Score: " + playerScript.playerScore;
+
+        }
+        
+
+        //wasPaused makes it so theres a frame before the pause menu closes
+        wasPaused = GamePaused;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
     public void LoadDebugScene()
     {
         SceneManager.LoadScene("DebugScene");
@@ -17,19 +87,58 @@ public class UiManager : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
-    //loads the next scene in build settings
-    //will be updated so we can randomly load scenes
-    public void LoadNextScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-
     //Quits game intended to use on a button
     public void QuitGame()
     {
         Application.Quit();
     }
 
-   
+
+    public void Resume()
+    {
+        if (pauseMenuUI != null)
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1.0f;
+            GamePaused = false;
+        }
+    }
+
+    //Completly pauses the game setting the games time to zero and the pause menu to be active
+    public void Pause()
+    {
+        if (pauseMenuUI != null)
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0;
+            GamePaused = true;
+        }
+    }
+
+
+
+
+    public void OptionsButton()
+    {
+        if (Ui1 != null)
+        {
+            Ui1.SetActive(false);
+            optionsMenu.SetActive(true);
+            Time.timeScale = 0f;
+            Debug.Log("onClickOptions CLicked");
+        }
+
+    }
+
+    public void BackButton()
+    {
+        if (Ui1 != null)
+        {
+            Ui1.SetActive(true);
+            optionsMenu.SetActive(false);
+            Time.timeScale = 0f;
+            Debug.Log("BACK CLICKED");
+        }
+    }
+
 }
