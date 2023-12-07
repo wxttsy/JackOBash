84896550104923185d9721e4812a931ce70ece58
benxv1 <132529015+benxv1@users.noreply.github.com>
2023-12-07 14:57:59 +1100

    Splash Screen (new)
    Splash screen now operates with control input and newer images have been implemented.

--- Assets/Scripts/Ui/SplashScreen.cs
+++ Assets/Scripts/Ui/SplashScreen.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -8,32 +9,33 @@ public class SplashScreen : MonoBehaviour
 {
     // Start is called before the first frame update
 
-
+    public EventSystem eventSystem;
+    public GameObject continueButton;
     public GameObject splash1;
     public GameObject splash2;
-    int splashIndex; 
+    int splashIndex;
     void Start()
     {
         splashIndex = 0;
         splash1.SetActive(true);
         splash2.SetActive(false);
+        eventSystem.SetSelectedGameObject(continueButton);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Next()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (splashIndex == 0)
         {
-            if (splashIndex == 0)
-            {
-                splash1.SetActive(false);
-                splash2.SetActive(true);
-                splashIndex = 1;
-            }
-            else if (splashIndex == 1)
-            {
-                SceneManager.LoadScene("DebugScene");
-            }
+            splash1.SetActive(false);
+            splash2.SetActive(true);
+            splashIndex = 1;
+        }
+        else if (splashIndex == 1)
+        {
+            SceneManager.LoadScene("DebugScene");
         }
     }
+
 }
+    
+    
